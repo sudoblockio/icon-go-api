cfe77bfce7de2598c5ba6a2c6da860b1ca29a525
robcxyz <rob@sudoblock.io>
2023-04-22 10:43:36 +0700

    fix: bump the MaxPageSkip to 1.5M

--- src/config/config.go
+++ src/config/config.go
@@ -27,7 +27,7 @@ type configType struct {
 
 	// Endpoints
 	MaxPageSize int `envconfig:"MAX_PAGE_SIZE" required:"false" default:"100"`
-	MaxPageSkip int `envconfig:"MAX_PAGE_SKIP" required:"false" default:"1000000"`
+	MaxPageSkip int `envconfig:"MAX_PAGE_SKIP" required:"false" default:"1500000"`
 
 	// CORS
 	CORSAllowOrigins  string `envconfig:"CORS_ALLOW_ORIGINS" required:"false" default:"*"`
